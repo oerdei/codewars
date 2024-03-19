555678c01a43470d53842f5e2ae339110c5015b7
oerdei <octavian@erdei.ro>
2024-03-19 03:43:57 +0000

    new modification

--- say_hello.py
+++ say_hello.py
@@ -1,6 +1,14 @@
 def say_hello(name, city, state):
     print(len(name))
-    return f'Hello, {name[0]} {name[1]}! Welcome to {city}, {state}!'
+    #a = name[0]
+    #for x in range(len(name)):
+    #   new_name = a.join(name[x])
+    x = 1
+    new_name = name[0]
+    while x < len(name):
+        new_name = new_name + " " + name[x]
+        x = x + 1
+    return f'Hello, {new_name}! Welcome to {city}, {state}!'
     
     
-print(say_hello(['John', 'Smith'], 'Phoenix', 'Arizona'))
+print(say_hello(['Franklin','Delano','Roosevelt','Otto'], 'Phoenix', 'Arizona'))
