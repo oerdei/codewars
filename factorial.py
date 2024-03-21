5eb8c6474ef008af81a5b233a0e7c5d41f5558e7
oerdei <octavian@erdei.ro>
2024-03-21 03:52:03 +0000

    script modified 2

--- factorial.py
+++ factorial.py
@@ -1,16 +1,16 @@
 def factorial(n):
-    if n > 0 and n < 12:
+    if n >= 0 and n <= 12:
         if n == 0:
             return 1
         else:
             i = 1
             s = 1
-            while i <= n:
-                s = s * (s + 1)
+            while i < n:
+                s = s * (i + 1)
                 i = i + 1
             return s
     else:
         return ValueError
                 
-n = 5
+n = -32
 print(factorial(n))
